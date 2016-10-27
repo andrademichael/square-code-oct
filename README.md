d8bbffd6b3aff56cab8f177d8c65c8f023d12e56
Michael E. Andrade <andrade.michael.e@gmail.com>
2016-10-27 07:28:16 -0700

    cleanup README and comment JS

--- README.md
+++ README.md
@@ -8,11 +8,17 @@ Behaviors:
 
 *DONE returns the edge length of the largest perfect square whose area is equal to or larger than # of characters in only-alphachars string*
 
-rearranges chars according to square encoding method (prints [0]th, [squareSize]th, [2 * squareSize]th, [3 * squareSize]th... characters up to [squareSize ^ 2]th, then 2nd, [squareSize]+1)th, (2x+1)th, (3x+1)th...
-3rd, ...
+*DONE rearranges chars according to square encoding method*
+
+<!-- row 1: prints [0]th, [squareSize]th, [2 * squareSize]th, [3 * squareSize]th... characters up to [squareSize ^ 2]th
 .
-.
-.
-until out of chars.
+until out of chars. -->
+
+<!-- 1, 9, 17, 25, 33, 41, 49, 57, 65,
+2, 10, 18, 26,
+
+start at (colNum)th, print every (squareSize)th until hit an empty,
+start at (colNum + rowNum), print every (squareSize)th...
+start at (colNum + rowNumb)* -->
 
 *DONE Prints out new encoded string.*
--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
   //form submission
   $("form#inputForm").submit(function(event) {
     event.preventDefault();
-    //input should be integer only
+    //input may be anything, non alpha characters will be removed
     var input = $("#inputText").val();
-    //result will be a boolean
+    //result will be the new, encoded string
     var result = encodeSquare(input);
     $("#result").text(result);
   });
@@ -15,25 +15,29 @@ $(document).ready(function() {
 //backend
 
 var encodeSquare = function(text) {
+  //textStripped is the input text minus spaces and non alpha characters
   var textStripped = text.replace(/[\s\W]+/g, "");
-  console.log(textStripped);
+  //textCharCount is the length of textStripped
   var textCharCount = textStripped.length;
-  console.log(textCharCount);
+  //squareSize is the largest perfect square that textStripped will fill completely, with overflow allowed up until the next largets perfect square - 1.
   var squareSize = Math.ceil(Math.sqrt(textCharCount));
-  console.log(squareSize);
 
+  //encodedText will be used for output, and will have characters added in the new order inside the loop
   var encodedText = "";
-  debugger;
+  //first for loop simulates columns of the square
   for (colNum = 1; colNum <= squareSize; colNum ++) {
     console.log("colNum is now " + colNum);
+    // inner for loop simulates rows
     for (rowNum = 1; rowNum < textCharCount; rowNum ++) {
       console.log("rowNum is now " + rowNum);
       console.log("current character is " + textStripped.charAt(((colNum - 1) + ((rowNum - 1) * squareSize))));
+      //if we've reached an empty character past the end of textStripped, do nothing and since the index will be >= textCharCount, go back to the outer loop to increment colNum by 1 and restart
       if (!(textStripped.charAt(((colNum - 1) + ((rowNum - 1) * squareSize))))) {
         console.log("reached a falsey character in 'textStripped'");
       } else {
-      console.log("adding " + textStripped[(colNum - 1) + ((rowNum - 1) * squareSize)] + " to encodedText");
-      encodedText += textStripped[(colNum - 1) + ((rowNum - 1) * squareSize)];
+        //if we haven't reached the end of the textStripped, add the current character to the end of encodedText
+        console.log("adding " + textStripped[(colNum - 1) + ((rowNum - 1) * squareSize)] + " to encodedText");
+        encodedText += textStripped[(colNum - 1) + ((rowNum - 1) * squareSize)];
       };
     };
   };
